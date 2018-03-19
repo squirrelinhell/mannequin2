12caeb27eefc39f3b33c42547ebc1927e24cbacc
squirrelinhell <squirrelinhell@users.noreply.github.com>
2018-03-19 15:46:43 +0100

    Remove named tuple (big speedup)

--- mannequin/basicnet.py
+++ mannequin/basicnet.py
@@ -1,5 +1,4 @@
 
-import collections
 import numpy as np
 import mannequin.backprop as backprop
 
@@ -8,20 +7,9 @@ def endswith(a, b):
         return False
     return a[len(a)-len(b):] == b
 
-class Layer(collections.namedtuple("Layer", " ".join((
-        "inputs",
-        "evaluate",
-        "prerequisites",
-        "shape",
-        "n_params",
-        "n_local_params",
-        "get_params",
-        "load_params",
-        "get_last_gradient",
-    )))):
-
-    def __new__(cls, *inputs, evaluate, shape, n_local_params=0,
-            get_params=None, load_params=None, get_last_gradient=None):
+class Layer(object):
+    def __init__(self, *inputs, evaluate, shape, n_local_params=0,
+            get_params=None, load_params=None):
         inputs = tuple(inputs)
         shape = tuple(max(1, int(s)) for s in shape)
         n_local_params = max(0, int(n_local_params))
@@ -80,11 +68,14 @@ class Layer(collections.namedtuple("Layer", " ".join((
                         pos += layer.n_local_params
                 assert pos == n_params
 
-        return super().__new__(
-            cls, inputs, evaluate, tuple(prerequisites),
-            shape, n_params, n_local_params,
-            get_params, load_params, get_last_gradient
-        )
+        self.inputs = inputs
+        self.evaluate = evaluate
+        self.prerequisites = tuple(prerequisites)
+        self.shape = shape
+        self.n_params = n_params
+        self.n_local_params = n_local_params
+        self.get_params = get_params
+        self.load_params = load_params
 
     def __call__(self, *args, **kwargs):
         outs, _ = self.evaluate(*args, **kwargs)
@@ -97,173 +88,174 @@ class Layer(collections.namedtuple("Layer", " ".join((
         return Multiply(self, other)
 
     def __str__(self):
-        return "<Layer n_inputs=%s n_params=%d>" % (
-            len(self.inputs), self.n_params
-        )
+        return "<%s: %s>" % (self.__class__.__name__, self.shape)
 
     __repr__ = __str__
 
-def Input(*shape):
-    last_gradient = None
+class Input(Layer):
+    def __init__(self, *shape):
+        self.last_gradient = None
 
-    def backprop(grad, output=None):
-        nonlocal last_gradient
-        assert endswith(grad.shape, shape)
-        grad.setflags(write=False)
-        last_gradient = grad[:]
-        if output is None:
-            return []
-        else:
-            assert len(output) == 0
-            return output
+        def backprop(grad, output=None):
+            assert endswith(grad.shape, shape)
+            grad.setflags(write=False)
+            self.last_gradient = grad[:]
 
-    def evaluate(array):
-        array = np.asarray(array, dtype=np.float32)
-        assert endswith(array.shape, shape)
-        return array, backprop
-
-    return Layer(evaluate=evaluate, shape=shape,
-        get_last_gradient=lambda: last_gradient)
-
-def Const(value):
-    value = np.array(value, dtype=np.float32)
-    assert len(value.shape) >= 1
-    value.setflags(write=False)
-
-    def backprop(grad, output=None):
-        assert endswith(grad.shape, shape)
-        if output is None:
-            return []
-        else:
-            assert len(output) == 0
-            return output
-
-    def evaluate(array):
-        return value[:], backprop
-
-    return Layer(evaluate=evaluate, shape=value.shape)
-
-def Params(*shape, init=np.zeros):
-    shape = tuple(max(1, int(s)) for s in shape)
-    assert len(shape) >= 1
-
-    value = np.array(init(*shape), dtype=np.float32).reshape(shape)
-    value.setflags(write=False)
-
-    def backprop(grad, output=None):
-        if output is None:
-            return grad.reshape(value.size)
-        else:
-            output[:] = grad.reshape(value.size)
-            return output
-
-    def evaluate(array):
-        return value[:], backprop
-
-    def get(*, output=None):
-        if output is None:
-            return value.reshape(-1)
-        else:
-            output[:] = value.reshape(-1)
-            return output
-
-    def load(new_value):
-        value.setflags(write=True)
-        value[:] = new_value.reshape(shape)
-        value.setflags(write=False)
-
-    return Layer(evaluate=evaluate, shape=shape,
-        n_local_params=value.size, get_params=get, load_params=load)
-
-def Function(*args, f, shape=None):
-    assert len(args) >= 1
-
-    if shape is None:
-        shape = args[0].shape
-
-    def evaluate(inps, **kwargs):
-        if isinstance(inps, dict):
-            inps = [inps[a] for a in args]
-
-            # Require correct shapes
-            for i, a in zip(inps, args):
-                assert endswith(i.shape, a.shape)
-
-            f_value, f_backprop = f(*inps, **kwargs)
-            assert endswith(f_value.shape, shape)
-            f_value.setflags(write=False)
-
-            return f_value, f_backprop
-
-        else:
-            inps = np.asarray(inps, dtype=np.float32)
-            exec_order = self.prerequisites + (self,)
-            layer_outs = {}
-            layer_bpps = {}
-
-            for layer in exec_order:
-                out, bpp = layer.evaluate(
-                    layer_outs if len(layer.inputs) >= 1 else inps,
-                    **(kwargs if layer == self else {})
-                )
-                layer_outs[layer] = out
-                layer_bpps[layer] = bpp
-
-            def backprop(grad, *, output=None):
-                grad = np.asarray(grad, dtype=np.float32)
-                assert grad.shape == layer_outs[self].shape
-                batch_shape = grad.shape[:len(grad.shape)-len(shape)]
-
-                layer_grads = {self: grad}
-
-                for layer in reversed(exec_order):
-                    if len(layer.inputs) <= 0:
-                        continue
-
-                    inp_grads = layer_bpps[layer](layer_grads[layer])
-                    del layer_grads[layer]
-
-                    assert isinstance(inp_grads, tuple)
-                    assert len(inp_grads) == len(layer.inputs)
-
-                    for g, i in zip(inp_grads, layer.inputs):
-                        assert g.shape == layer_outs[i].shape
-
-                        g_batch = g.shape[:len(g.shape)-len(i.shape)]
-                        assert endswith(batch_shape, g_batch)
-
-                        if len(g_batch) < len(batch_shape):
-                            # Return batch average
-                            g = g / np.prod(
-                                batch_shape[:len(batch_shape)-len(g_batch)]
-                            )
-
-                        if i in layer_grads:
-                            layer_grads[i] = layer_grads[i] + g
-                        else:
-                            layer_grads[i] = g
-
-                if output is None:
-                    output = np.zeros(self.n_params, dtype=np.float32)
-                else:
-                    assert output.shape == (self.n_params,)
-
-                pos = 0
-                for layer in exec_order:
-                    if layer.n_local_params >= 1:
-                        layer_bpps[layer](
-                            layer_grads[layer],
-                            output=output[pos:pos+layer.n_params]
-                        )
-                        pos += layer.n_params
-                assert pos == self.n_params
-
+            if output is None:
+                return []
+            else:
+                assert len(output) == 0
                 return output
 
-            return layer_outs[self], backprop
+        def evaluate(array):
+            array = np.asarray(array, dtype=np.float32)
+            assert endswith(array.shape, shape)
+            return array, backprop
 
-    self = Layer(*args, evaluate=evaluate, shape=shape)
-    return self
+        super().__init__(evaluate=evaluate, shape=shape)
+
+class Const(Layer):
+    def __init__(self, value):
+        value = np.array(value, dtype=np.float32)
+        assert len(value.shape) >= 1
+        value.setflags(write=False)
+
+        def backprop(grad, output=None):
+            assert endswith(grad.shape, shape)
+
+            if output is None:
+                return []
+            else:
+                assert len(output) == 0
+                return output
+
+        def evaluate(array):
+            return value[:], backprop
+
+        super().__init__(evaluate=evaluate, shape=value.shape)
+
+class Params(Layer):
+    def __init__(self, *shape, init=np.zeros):
+        shape = tuple(max(1, int(s)) for s in shape)
+        assert len(shape) >= 1
+
+        value = np.array(init(*shape), dtype=np.float32).reshape(shape)
+        value.setflags(write=False)
+
+        def backprop(grad, output=None):
+            if output is None:
+                return grad.reshape(value.size)
+            else:
+                output[:] = grad.reshape(value.size)
+                return output
+
+        def evaluate(array):
+            return value[:], backprop
+
+        def get(*, output=None):
+            if output is None:
+                return value.reshape(-1)
+            else:
+                output[:] = value.reshape(-1)
+                return output
+
+        def load(new_value):
+            value.setflags(write=True)
+            value[:] = new_value.reshape(shape)
+            value.setflags(write=False)
+
+        super().__init__(evaluate=evaluate, shape=shape,
+            n_local_params=value.size, get_params=get, load_params=load)
+
+class Function(Layer):
+    def __init__(self, *args, f, shape=None):
+        assert len(args) >= 1
+
+        if shape is None:
+            shape = args[0].shape
+
+        def evaluate(inps, **kwargs):
+            if isinstance(inps, dict):
+                inps = [inps[a] for a in args]
+
+                # Require correct shapes
+                for i, a in zip(inps, args):
+                    assert endswith(i.shape, a.shape)
+
+                f_value, f_backprop = f(*inps, **kwargs)
+                assert endswith(f_value.shape, shape)
+                f_value.setflags(write=False)
+
+                return f_value, f_backprop
+
+            else:
+                inps = np.asarray(inps, dtype=np.float32)
+                exec_order = self.prerequisites + (self,)
+                layer_outs = {}
+                layer_bpps = {}
+
+                for layer in exec_order:
+                    out, bpp = layer.evaluate(
+                        layer_outs if len(layer.inputs) >= 1 else inps,
+                        **(kwargs if layer == self else {})
+                    )
+                    layer_outs[layer] = out
+                    layer_bpps[layer] = bpp
+
+                def backprop(grad, *, output=None):
+                    grad = np.asarray(grad, dtype=np.float32)
+                    assert grad.shape == layer_outs[self].shape
+                    batch_shape = grad.shape[:len(grad.shape)-len(shape)]
+
+                    layer_grads = {self: grad}
+
+                    for layer in reversed(exec_order):
+                        if len(layer.inputs) <= 0:
+                            continue
+
+                        inp_grads = layer_bpps[layer](layer_grads[layer])
+                        del layer_grads[layer]
+
+                        assert isinstance(inp_grads, tuple)
+                        assert len(inp_grads) == len(layer.inputs)
+
+                        for g, i in zip(inp_grads, layer.inputs):
+                            assert g.shape == layer_outs[i].shape
+
+                            g_batch = g.shape[:len(g.shape)-len(i.shape)]
+                            assert endswith(batch_shape, g_batch)
+
+                            if len(g_batch) < len(batch_shape):
+                                # Return batch average
+                                g = g / np.prod(
+                                    batch_shape[:len(batch_shape)-len(g_batch)]
+                                )
+
+                            if i in layer_grads:
+                                layer_grads[i] = layer_grads[i] + g
+                            else:
+                                layer_grads[i] = g
+
+                    if output is None:
+                        output = np.zeros(self.n_params, dtype=np.float32)
+                    else:
+                        assert output.shape == (self.n_params,)
+
+                    pos = 0
+                    for layer in exec_order:
+                        if layer.n_local_params >= 1:
+                            layer_bpps[layer](
+                                layer_grads[layer],
+                                output=output[pos:pos+layer.n_params]
+                            )
+                            pos += layer.n_params
+                    assert pos == self.n_params
+
+                    return output
+
+                return layer_outs[self], backprop
+
+        super().__init__(*args, evaluate=evaluate, shape=shape)
 
 def normed_columns(inps, outs):
     m = np.random.randn(inps, outs).astype(np.float32)
